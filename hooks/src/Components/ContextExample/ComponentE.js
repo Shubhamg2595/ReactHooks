539d6cbcf9b2d087c4cee725105ec26b9b27eeaf
Shubham Gupta <Shubhamg2595@gmail.com>
2020-05-14 23:06:45 +0530

    useContext Hook Done

--- hooks/src/Components/ContextExample/ComponentE.js
+++ hooks/src/Components/ContextExample/ComponentE.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import ComponentF from './ComponentF'
+import React, { useContext } from 'react';
+import { ChannelContext, UserContext } from '../../App'
+import ComponentF from './ComponentF';
+
+
 
 function ComponentE() {
+
+
+    const user = useContext(UserContext)
+    const channel = useContext(ChannelContext)
+
     return (
         <div>
             <p> ComponentE </p>
-            <ComponentF />
+            userName {user} - CreatedBy {channel}
         </div>
     )
 }
